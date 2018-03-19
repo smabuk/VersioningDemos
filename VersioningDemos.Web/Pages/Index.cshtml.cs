a49f1c6df6d150885634105ddab708e83d4802e9
Simon Brookes <simon.brookes@btinternet.com>
2018-03-19 15:08:07 +0000

    SemanticVersioning using version parts

--- VersioningDemos.Web/Pages/Index.cshtml.cs
+++ VersioningDemos.Web/Pages/Index.cshtml.cs
@@ -30,6 +30,7 @@ namespace VersioningDemos.Web.Pages
             p1 = new Manual.AllDifferent.Class1();
             p1 = new VersionPrefix.Class1();
             p1 = new VersionSuffix.Class1();
+            p1 = new SemanticVersioning.Class1();
 
             var assemblies = from a in AppDomain.
                 CurrentDomain.
